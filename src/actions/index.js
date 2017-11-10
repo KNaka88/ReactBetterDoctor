c4710c7acd2b1779719457434b7e82d6fec84bb4
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-11-09 17:57:21 -0800

    add input form for searchBar component

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export function searchDoctor() {
+  console.log("findDoctor");
+}
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { searchDoctor } from '../actions';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
+  renderField(field) {
+    return (
+      <div>
+        <label>Find Doctor</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
+
   render() {
     return (
-      <h1>Hello World</h1>
+      <form>
+        <Field
+          label="Search Query"
+          name="query"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-success">Search</button>
+      </form>
     );
   }
 }
+
+export default reduxForm({
+  form: 'SearchDoctorForm'
+})(
+  connect(null, { searchDoctor })(SearchBar)
+);
