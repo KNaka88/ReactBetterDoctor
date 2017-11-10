8d8a13f78110851bb0019969424fee63c61ba40e
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-11-09 19:24:28 -0800

    add api request functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,16 @@
 import axios from 'axios';
+import API_KEY from '../../api_key';
 
-export function searchDoctor() {
-  console.log("findDoctor");
+export const FIND_DOCTOR = 'find_doctor';
+const ROOT_URL = "https://api.betterdoctor.com/2016-03-01/doctors?user_key=";
+
+
+
+
+export function searchDoctor(value, callback) {
+  const request = axios.get(`${ROOT_URL}${API_KEY}&query=${value}&&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10`);
+  return {
+    type: FIND_DOCTOR,
+    payload: request
+  }
 }
---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,10 @@
+import { FIND_DOCTOR } from '../actions';
+
+export default function(state = {}, action) {
+  switch (action.type) {
+    case FIND_DOCTOR:
+      return[ action.payload.data.data, ...state];
+    default:
+      return state;
+  }
+}
