66a934a0787883e153f3c59d6b73b9dc8fdbb08c
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-11-18 21:13:32 -0800

    create result component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar';
+import ResultList from '../containers/result_list';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <SearchBar />
+        <ResultList />
       </div>
     );
   }
