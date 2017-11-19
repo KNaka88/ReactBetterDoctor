ad5d2356e4af740455ebe4654d54790cc3a3a031
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-11-18 22:01:14 -0800

    change ResultList component as class and display state

--- src/containers/result_list.js
+++ src/containers/result_list.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
 
 
-export default function () {
-  return (
-    <div>
-      <p>Result</p>
-    </div>
-  );
+
+class ResultList extends Component {
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      console.log(post);
+      return(
+        <li key={post.uid}>Hey</li>
+      )
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <p>Result</p>
+        {this.renderPosts()}
+      </div>
+    );
+  }
+
 }
+
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps)(ResultList);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -3,7 +3,7 @@ import { FIND_DOCTOR } from '../actions';
 export default function(state = {}, action) {
   switch (action.type) {
     case FIND_DOCTOR:
-      return[ action.payload.data.data, ...state];
+      return action.payload.data.data;
     default:
       return state;
   }
