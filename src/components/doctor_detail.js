67af3654538db45b68ee99407a9079ae9e96cc40
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-11-18 23:01:46 -0800

    add DoctorDetail component

---
+++ src/components/doctor_detail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class DoctorDetail extends Component {
+  render(){
+    return (
+      <p>This is Doctor Detail Page</p>
+    )
+  };
+}
+
+export default DoctorDetail;
